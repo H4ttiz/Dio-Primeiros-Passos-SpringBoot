1434e6c803688fed109df525450455b03c7a616b
leonardo <leonardocbizerra@gmail.com>
2025-04-07 15:11:45 -0300

    Primeiros-Passos-SpringBoot

---
+++ Primeiros-Passos/src/main/java/com/leo/Primeiros_Passos/Calculadora.java
@@ -0,0 +1,10 @@
+package com.leo.Primeiros_Passos;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Calculadora {
+    public int soma(int a, int b) {
+        return a + b;
+    }
+}
